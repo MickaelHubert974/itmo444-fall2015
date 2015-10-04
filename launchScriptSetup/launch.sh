9bb0c980b3a2fc27056ec7092189acda5884f0c0
MickaelHubert974 <mickaelhubert.974@gmail.com>
2015-10-03 22:41:05 -0500

    launch setup added

--- launchScriptSetup/launch.sh
+++ launchScriptSetup/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --key-name $6 --user-data https://github.com/MickaelHubert974/itmo444-fall2015/blob/master/environmentSetup/install-env.sh --profile mickaelhubert
+
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --key-name $6 --user-data file://../environmentSetup/install-env.sh --profile mickaelhubert
 
