750c146e55dd55c239d72d522c9368998f8bc4a6
MickaelHubert974 <mickaelhubert.974@gmail.com>
2015-10-21 23:49:09 -0500

    launch script ok

--- MP1/launchScriptSetup/launch.sh
+++ MP1/launchScriptSetup/launch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#launch configuration
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --key-name $6 --user-data file://../environmentSetup/install-env.sh --iam-instance-profile $7
+declare -a instancesID
 
-aws ec2 wait instance-running
+mapfile -t instancesID < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --key-name $6 --user-data file://../environmentSetup/install-env.sh --iam-instance-profile Name=phpdevelopperRole --output table | grep InstanceId | sed "s/|//g" | sed "s/ //g" | sed "s/InstanceId//g")
+
+
+echo ${instancesID[@]}
+
+aws ec2 wait instance-running --instance-ids ${instancesID[@]}
+
+
