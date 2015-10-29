49ee4fe06eff116248211f28fee883ecde50767a
MickaelHubert974 <mickaelhubert.974@gmail.com>
2015-10-29 10:43:00 -0500

    db creation moved back to launch-all lol

--- MP1/environmentSetup/install-webserver.sh
+++ MP1/environmentSetup/install-webserver.sh
@@ -11,26 +11,6 @@ sudo cp ~/itmo444-fall2015/MP1/applicationSetup/images /var/www/html/images
 sudo cp ~/itmo444-fall2015/MP1/applicationSetup/*.html /var/www/html
 sudo cp ~/itmo444-fall2015/MP1/applicationSetup/*.php /var/www/html
 
-mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
-
-if [ ${#dbInstanceARR[@]} -gt 0 ]
-   then
-   echo "Chekcing RDS database-instances"
-   LENGTH=${#dbInstanceARR[@]}
-
-      for (( i=0; i<${LENGTH}; i++));
-      do
-      if [ ${dbInstanceARR[i]} == "itmo444-db" ] 
-     then 
-      echo "db exists"
-     else
-     aws rds create-db-instance --db-instance-identifier itmo444-db --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein42 --allocated-storage 5 --db-name customerrecords
-     
-      fi  
-     done
-fi
-
-wait db-instance-available --db-instance-identifier itmo444-db
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
